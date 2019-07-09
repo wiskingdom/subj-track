fb436e6f42300996d638d7bf94f07399443e9196
Yeonwoo Kim <wiskingdom@gmail.com>
2019-07-09 23:53:31 +0900

    Reset gitignore

--- src/fireapp.js
+++ src/fireapp.js
@@ -1,6 +1,6 @@
 import * as firebase from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/database';
-import firebaseConfig from './firebase-config.json';
+import firebaseConfig from './.firebase.config.json';
 
 export default firebase.initializeApp(firebaseConfig);

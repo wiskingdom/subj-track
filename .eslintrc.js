9836f459a6396ede2d81babaa9c66e8cb733df43
Yeonwoo Kim <wiskingdom@gmail.com>
2019-07-11 12:02:59 +0900

    Change for no-unused-vars

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,6 +11,7 @@ module.exports = {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-param-reassign': 'off',
+    'no-unused-vars': ['error', { 'argsIgnorePattern': '^_' }],
   },
   parserOptions: {
     parser: 'babel-eslint',
